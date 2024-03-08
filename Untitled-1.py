341a91a5793a5e142360dca5f63358630176b2ed
Mona Lisa <sania5352@gmail.com>
2024-03-08 21:19:14 +0200

    обрахунок дат

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,8 @@
-print("hello")
+from datetime import datetime , timedelta
+
+#def get_days_from_today():
+today = datetime.now() 
+day_now = today.date()
+past_day = datetime(year=2020, month=10, day=9)
+result = past_day.toordinal() - day_now.toordinal()
+print(result)
