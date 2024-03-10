223982c21e2d699b97fed69bb79876575c92af4c
Mona Lisa <sania5352@gmail.com>
2024-03-10 12:19:00 +0200

    hw_1

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,8 +1,13 @@
 from datetime import datetime , timedelta
 
-#def get_days_from_today():
-today = datetime.now() 
-day_now = today.date()
-past_day = datetime(year=2020, month=10, day=9)
-result = past_day.toordinal() - day_now.toordinal()
-print(result)
+
+
+def get_days_from_today(past_day):
+    today = datetime.now() 
+    day_now = today.date()
+    past_day = datetime.strptime(date, "%Y-%m-%d").date()
+    result = past_day.toordinal() - day_now.toordinal()
+    return result
+    
+date = input("Enter the date in YYYY-MM-DD format: ")
+get_days_from_today(date)
